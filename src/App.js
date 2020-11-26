a35972077f937b42f498551d27b5b00cd2e4e512
Pidgey Sandoval <jcs666a2000@gmail.com>
2020-11-26 13:03:04 -0600

    some improvements

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import { getStates } from './fakeAPI';
 import './App.css';
 
 function App() {
-  const [state, filterStates] = useState({
+  const [state, updateState] = useState({
     isLoading: true,
     inputText: '',
     stateList: []
   });
 
   const {
+    inputText,
     isLoading,
     stateList
   } = state;
 
-  const getFunc = async () => {
+  const getStatesList = async () => {
     const response = await getStates();
     const filteredResponse = response.filter(res => {
-      return res.name.toUpperCase().includes(state.inputText.toUpperCase())
+      return res.name.toUpperCase().includes(inputText.toUpperCase())
     });
-    filterStates((prevState) => ({
+    updateState((prevState) => ({
       ...prevState,
       isLoading: false,
       stateList: filteredResponse
@@ -27,10 +28,10 @@ function App() {
   };
 
   const setLoading = () =>
-    filterStates((prevState) => ({...prevState, isLoading: true, stateList: []}));
+    updateState((prevState) => ({...prevState, isLoading: true, stateList: []}));
 
   useEffect(() => {
-    getFunc();
+    getStatesList();
   }, [state.stateList]);
 
   return (
@@ -39,30 +40,25 @@ function App() {
       {isLoading && <h3>Loading...</h3>}
 
       <div className="field-container">
-        <input value={state.inputText}
-          onKeyPress={({target, key}) => {
+        <input value={inputText}
+          onKeyPress={({key}) => {
             if(key === 'Enter'){
               setLoading();
-              const inputText = target.value;
-              filterStates((prevState) => ({...prevState, inputText}));
             }
           }}
           onChange={({target}) => {
             const inputText = target.value;
-            filterStates((prevState) => ({...prevState, inputText}));
+            updateState((prevState) => ({...prevState, inputText}));
           }} />
-        <button onClick={()=> {
-          setLoading();
-          getFunc();
-        }} disabled={isLoading}>
+        <button onClick={setLoading} disabled={isLoading}>
           Filter
         </button>
       </div>
 
       {!isLoading && <ul>
-        {stateList.map(state => (
-        <li key={state.id}>
-          {state.name}
+        {stateList.map(currState => (
+        <li key={currState.id}>
+          {currState.name}
         </li>))}
       </ul>}
     </div>
